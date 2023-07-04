22653cbaa3b39e61125f8f528fee3896b03e0208
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-07-04 10:10:04 +1000

    fix: remove host variable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const {app, HOST, PORT} = require("./server");
+const {app, PORT} = require("./server");
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
 });
